24d71006662b0134c33f0450758bf86a68ed9849
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-07-03 18:15:42 +0530

    tests and dependencies added

--- app/src/androidTest/java/com/example/cryptile/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/cryptile/ExampleInstrumentedTest.kt
@@ -1,22 +1,56 @@
 package com.example.cryptile
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    private val sleepSmall: Long = 3_000
+    private val sleepLong: Long = 8_000
+    private val sleepEmailVerification: Long = 30_000
+
+    private val email = "vaishnav.kanhira@gmail.com"
+    private val accountPassword = "12345678"
+    private val userName = "MyUserName"
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.cryptile", appContext.packageName)
+    fun signUpAndEnter() {
+        signUpWithEmail()
+        Thread.sleep(sleepEmailVerification)
+        loginUsingEmail()
+    }
+
+    /**
+     * run while on sign in screen
+     */
+    private fun signUpWithEmail() {
+        onView(withId(R.id.email_sign_up_button)).perform(click())
+        onView(withId(R.id.user_name_edit_text)).perform(typeText(userName))
+        onView(withId(R.id.user_email_edit_text)).perform(typeText(email))
+        onView(withId(R.id.user_password_edit_text)).perform(typeText(accountPassword))
+        onView(withId(R.id.user_confirm_password_edit_text)).perform(typeText(accountPassword))
+        onView(withId(R.id.sign_up_button)).perform(click())
+        Thread.sleep(sleepSmall)
+        onView(withId(R.id.dismiss_button)).perform(click())
+    }
+
+    /**
+     * run while on sign in screen
+     */
+    private fun loginUsingEmail() {
+        onView(withId(R.id.user_email_edit_text)).perform(typeText(email))
+        onView(withId(R.id.user_password_edit_text)).perform(typeText(accountPassword))
+        onView(withId(R.id.login_button)).perform(click())
     }
 }
