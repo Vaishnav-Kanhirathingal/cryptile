2819ed7ef3344132cc46ef77b4ad94910cdf4dbe
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-07-04 18:02:40 +0530

    settings fragment listeners added

--- app/src/main/java/com/example/cryptile/ui_fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/cryptile/ui_fragments/SettingsFragment.kt
@@ -1,26 +1,39 @@
 package com.example.cryptile.ui_fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.asLiveData
 import androidx.navigation.fragment.findNavController
+import com.example.cryptile.app_data.AppApplication
 import com.example.cryptile.app_data.data_store_files.AppDataStore
 import com.example.cryptile.app_data.data_store_files.StoreBoolean
 import com.example.cryptile.databinding.FragmentSettingsBinding
+import com.example.cryptile.ui_fragments.prompt.AdditionalPrompts
+import com.example.cryptile.view_models.AppViewModel
+import com.example.cryptile.view_models.AppViewModelFactory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+private const val TAG = "SettingsFragment"
+
 class SettingsFragment : Fragment() {
+    private val viewModel: AppViewModel by activityViewModels {
+        AppViewModelFactory((activity?.application as AppApplication).database.safeDao())
+    }
     private lateinit var binding: FragmentSettingsBinding
     private lateinit var dataStore: AppDataStore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSettingsBinding.inflate(layoutInflater)
         return binding.root
     }
@@ -33,6 +46,13 @@ class SettingsFragment : Fragment() {
 
     private fun applyBinding() {
         binding.apply {
+            newUserNameTextLayout.editText!!.setText(viewModel.userDisplayName.value)
+            dataStore.keepMeSignedInFlow.asLiveData().observe(viewLifecycleOwner) {
+                keepMeSignedInSwitch.isChecked = it
+            }
+            dataStore.fingerprintAppLockFlow.asLiveData().observe(viewLifecycleOwner) {
+                useFingerprintSwitch.isChecked = it
+            }
             topAppBar.setNavigationOnClickListener { findNavController().navigateUp() }
             keepMeSignedInSwitch.setOnCheckedChangeListener { _, isChecked ->
                 CoroutineScope(Dispatchers.IO).launch {
@@ -44,17 +64,43 @@ class SettingsFragment : Fragment() {
                     dataStore.booleanSaver(isChecked, StoreBoolean.USER_USES_FINGERPRINT)
                 }
             }
-            newUserNameTextLayout.setEndIconOnClickListener {
-                // TODO: change display name
+            newUserNameTextLayout.apply {
+                this.setEndIconOnClickListener {
+                    val lCheck = this.editText!!.text.length in 7..33
+                    Log.d(TAG, "lCheck = $lCheck")
+                    if (lCheck) {
+                        AdditionalPrompts.verifyUser(
+                            layoutInflater = layoutInflater,
+                            context = requireContext(),
+                            notice = "change user name",
+                            onSuccess = {
+                                // TODO: change display name
+                                Log.d(TAG, "user  name changed")
+                            }
+                        )
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "length should be 8-32 characters",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+            forgotPasswordImageButton.setOnClickListener {
+                // TODO: password reset email and message
+            }
+            ClearPasswordImageButton.setOnClickListener {
+                newPasswordTextLayout.editText!!.setText("")
+                newPasswordRepeatTextLayout.editText!!.setText("")
             }
             confirmPasswordImageButton.setOnClickListener {
-                // TODO: check old password
                 val p1Check: Boolean
                 val passwordOne: String
                 newPasswordTextLayout.apply {
                     passwordOne = this.editText!!.text.toString()
                     p1Check = passwordOne.length in 7..33
-                    this.error = "Password length should be 8-32 characters"
+                    this.error = "length should be 8-32 characters"
                     this.isErrorEnabled = !p1Check
                 }
                 val p2Check: Boolean
@@ -64,8 +110,26 @@ class SettingsFragment : Fragment() {
                     this.isErrorEnabled = !p2Check
                 }
                 if (p1Check && p2Check) {
-                    // TODO: use old and new password to change password
+                    AdditionalPrompts.verifyUser(
+                        layoutInflater = layoutInflater,
+                        context = requireContext(),
+                        notice = "Change password",
+                        onSuccess = {
+                            Log.d(TAG, "password changed")
+                            // TODO: use old and new password to change password
+                        }
+                    )
                 }
+            }
+            deleteAccountButton.setOnClickListener {
+                AdditionalPrompts.verifyUser(
+                    layoutInflater = layoutInflater,
+                    context = requireContext(),
+                    notice = "Change password",
+                    onSuccess = {
+                        // TODO: delete account
+                    }
+                )
             }
             // TODO: account settings for password reset, display name reset, delete for email
             // TODO: account settings for gmail as delete account
