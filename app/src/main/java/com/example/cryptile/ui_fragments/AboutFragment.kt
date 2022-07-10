21fe9848d6137445706a6c0c9109ee50900be9ea
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-07-10 20:24:27 +0530

    onclick listeners for about segment added

--- app/src/main/java/com/example/cryptile/ui_fragments/AboutFragment.kt
+++ app/src/main/java/com/example/cryptile/ui_fragments/AboutFragment.kt
@@ -1,6 +1,9 @@
 package com.example.cryptile.ui_fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +11,10 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.cryptile.databinding.FragmentAboutBinding
 
+private const val TAG = "AboutFragment"
+
 class AboutFragment : Fragment() {
     private lateinit var binding: FragmentAboutBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +35,17 @@ class AboutFragment : Fragment() {
             topAppBar.setNavigationOnClickListener {
                 findNavController().navigateUp()
             }
+            projectGithubButton.setOnClickListener { open("https://github.com/Vaishnav-Kanhirathingal/CRYPTILE") }
+            personalWhatsappButton.setOnClickListener { open("https://wa.me/917219648837") }
+            personalGithubButton.setOnClickListener { open("https://github.com/Vaishnav-Kanhirathingal") }
+            personalGmailButton.setOnClickListener { open("mailto:vaishnav.kanhira@gmail.com") }
+            personalOutlookButton.setOnClickListener { open("mailto:vaishnav.kanhira@outlook.com") }
+            personalInstagramButton.setOnClickListener { open("https://www.instagram.com/vaishnav_k.p/") }
         }
+    }
+
+    private fun open(url: String) {
+        Log.d(TAG, "opening url $url")
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
     }
 }
