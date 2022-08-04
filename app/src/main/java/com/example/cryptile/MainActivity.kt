b95d120040ef71c104baee37002783795cb1dfb1
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-08-04 19:35:39 +0530

    theme changed

--- app/src/main/java/com/example/cryptile/MainActivity.kt
+++ app/src/main/java/com/example/cryptile/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.cryptile
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.cryptile.databinding.ActivityMainBinding
@@ -16,8 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         navController = navHostFragment.navController
